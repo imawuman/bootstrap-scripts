a2bf87c810f8b88f80fb4839ed5447cfda85841d
Richard Wu <richard.liang.wu@gmail.com>
2017-11-19 18:36:38 -0800

    Add javascript readFile.js

--- javascript/commandLine.js
+++ javascript/commandLine.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 /**
- * Usage: ./commandLine.js arg1, arg2, ...
+ * Usage: ./commandLine.js [arg1] [arg2] ...
  */
-var params = process.argv.slice(2);
+
+const params = process.argv.slice(2);
 
 function commandLine(arg) {
     console.log("Running function with arg: " + arg);
---
+++ javascript/readFile.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/**
+ * Usage: ./readFile.js [filepath]
+ */
+
+const fs = require("fs");
+
+const params = process.argv.slice(2);
+
+if (params.length > 0) {
+    const filepath = params[0];
+    const lines = fs.readFileSync(filepath, "utf8").split("\n");
+    processFileLines(lines);
+} else {
+    console.error("Missing file argument.");
+}
+
+function processFileLines(lines) {
+    console.log("Printing file lines:")
+    lines.forEach((line) => console.log(line));
+}
